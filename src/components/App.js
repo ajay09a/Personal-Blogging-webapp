729b8da285ff6bb322a07c17dc21bd71e5b5561b
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-04-01 19:26:08 +0530

    about me section added

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Navbar from './Navbar'
 import CreatePost from './CreatePost';
 import Home from './Home';
 import PostDetail from './PostDetail';
+import AboutMe from './AboutMe';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home/>} />
         <Route exact path="/post/:postId" element={<PostDetail/>} />
         <Route exact path="/create-post" element={<CreatePost/>} />
+        <Route exact path="/aboutme" element={<AboutMe/>} />
       </Routes>
     </BrowserRouter>
   );
